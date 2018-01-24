ee30302a24416f63bb5282f1e8692b46224faf27
Valentin Gorlier <valentin.gorlier@hotmail.fr>
2018-01-24 16:42:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const Discord = require('discord.js')
+const cheerio = require('cheerio')
 const bot = new Discord.Client()
 var request = require('request')
+var tierRankText;
+var LeaguePointText;
+var winsText;
+var lossesText;
+var winratioText;
 
 var url = "http://euw.op.gg/summoner/userName=Yieldannn"
-    request(url, err, resp, body){
+request(url, function(err, resp, body){
     var $ = cheerio.load(body);
-    var tierRank = $('tierRank');
-    var tierRank = tierRank.text();
-    console.log("Coucou");
-    console.log(tierRank);
-}
+    var tierRank = $('.tierRank');
+    tierRankText = tierRank.text();
 
+    var LeaguePoint = $('.LeaguePoint');
+    LeaguePointText = LeaguePoint.text();
+
+    var wins = $('.wins');
+    winsText = wins.text();
+
+    var losses = $('.losses');
+    lossesText = losses.text();
+
+    var winratio = $('.winratio');
+    winratioText = winratio.text();
+});
 
 bot.login('NDA1NTI5ODc4MTM4NTg1MDg4.DUlyVw.dIRclJcJUe0ny7njGORq-T1_Y2c')
 bot.on('message', function (message) {
     if (message.content === '!elo') {
-        message.channel.send(document.getElementsByClassName('tierRank')[0].innerHTML);
+        message.channel.send(tierRankText,LeaguePointText,winsText,lossesText,winsText);
     }
 });
 
