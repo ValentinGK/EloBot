fff51c6218a67224ec8a7d83585eae40db1d69d6
Valentin Gorlier <valentin.gorlier@hotmail.fr>
2018-01-24 18:28:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ var LeaguePointsText;
 var winsText;
 var lossesText;
 var winratioText;
-var url = "http://euw.op.gg/summoner/userName="+
-function getInfo(nickname) {
+var url = "http://euw.op.gg/summoner/userName="
+function getInfo(nickname,message) {
     request(url+nickname, function(err, resp, body){
         var $ = cheerio.load(body);
         var tierRank = $('.tierRank');
         tierRankText = tierRank.text();
         var LeaguePoints = $('.LeaguePoints');
-        LeaguePointsText = LeaguePoints.text();
+        LeaguePointsText = LeaguePoints.text().replace(/\s/g,'');
         var wins = $('.wins');
         winsText = wins.text();
         var losses = $('.losses');
         lossesText = losses.text();
         var winratio = $('.winratio');
         winratioText = winratio.text();
-    });
+        if (tierRankText == 'Unranked'){
+            message.channel.send('This player is unranked');
+        }
+        else {
+            message.channel.send(tierRankText + " - " + LeaguePointsText + "\n" + winsText + " - " + lossesText + "\n" + winratioText);
+        }});
 };
 
 bot.login('NDA1NTI5ODc4MTM4NTg1MDg4.DUlyVw.dIRclJcJUe0ny7njGORq-T1_Y2c');
@@ -29,8 +34,7 @@ bot.on('message', function (message) {
     if (message.content.startsWith('!elo'))
     {
         var nickname = message.content.substr(5);
-        getInfo(nickname);
-        console.log(url,nickname,tierRankText);
-        message.channel.send(tierRankText + " - " + LeaguePointsText + "\n" + winsText + " - " + lossesText + "\n" + winratioText);
+        getInfo(nickname,message);
+        console.log(url + nickname,tierRankText);
     }
 });
