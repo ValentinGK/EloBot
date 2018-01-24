b62618df5bec6d2b54fcfb44ad056f80a73a304a
Valentin Gorlier <valentin.gorlier@hotmail.fr>
2018-01-24 23:10:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getInfo(nickname,message,server) {
     if (server == 'kr'){
         server = 'www'
     }
-    request(encodeUrl("http://" + server + url+nickname.replace(/\s/g,'')),console.log(encodeUrl("http://" + server + url+nickname), function(err, resp, body){
+    request(encodeUrl("http://" + server + url+nickname.replace(/\s/g,'')), function(err, resp, body){
         var $ = cheerio.load(body);
         var tierRank = $('.tierRank');
         tierRankText = tierRank.text();
@@ -33,14 +33,14 @@ function getInfo(nickname,message,server) {
         }
         if(tierRankText != "Unranked" && tierRankText){
             message.channel.send(tierRankText + " - " + LeaguePointsText + "\n" + winsText + " - " + lossesText + "\n" + winratioText);
-        }}));
+        }});
 };
 
 bot.login('NDA1NTI5ODc4MTM4NTg1MDg4.DUlyVw.dIRclJcJUe0ny7njGORq-T1_Y2c');
 bot.on('message', function (message) {
     try{
         if (message.content === '!help'){
-            message.channel.send('Maxence Roger')
+            message.channel.send('type !elo [nickname] [server] \n Server list: \n "kr" for Korea \n "jp" for Japan \n "na" for North America \n "eune" for \n "oce" for Oceania \n "br" for Brazil \n "las" for LAS \n "lan" for LAN \n "ru" for Russia \n "tr" for Turkey \n Leave empty [server] for EUW \n No white spaces in the nickname ! \n Example with Faker (Hide on bush): !elo Hideonbush kr')
         }
         if (message.content.startsWith('!elo'))
         {
